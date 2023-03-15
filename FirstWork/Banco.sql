8573c355a1ace75ee5cfe19a5df512c20c0a2854
renangxs <renangalgal@gmail.com>
2023-03-15 20:41:12 -0300

    att

--- FirstWork/Banco.sql
+++ FirstWork/Banco.sql
@@ -3,9 +3,9 @@ create database if not exists first_work;
 use first_work;
 
 create table if not exists usuario(
-id int not null auto_increment,
+id int primary key auto_increment,
 nome varchar(200) not null,
-CPF 
+CPF varchar(100) not null,
 telefone varchar(100) not null,
 Data_nascimento date not null,
 Endereco varchar(200) not null,
@@ -14,35 +14,35 @@ Estado varchar(100) not null,
 email varchar(100) not null,
 descr varchar(200) not null,
 img varchar(100) not null,
-rede_social varchar(100) not null,
+rede_social varchar(100),
 primary key (id));
 
 create table if not exists empresa(
-id int not null auto_increment,
-CNPJ 
+id int primary key auto_increment,,
+CNPJ varchar(100) not null,
 nome varchar(200) not null,
 Local varchar(100) not null,
 Hora_Funcionamento Int not null,
 Logo varchar(100) not null,
-Primary key(CNPJ));
+Primary key(id));
 
 create table if not exists vaga(
-id_vaga int not null auto_increment,
+id int primary key auto_increment,
 Cargo varchar(100) not null,
 Salario Float not null,
 carga_horaria Int not null,
 Requisitos varchar(250) not null,
 id_usuario int not null,
-Cnpj_emp int not null,
-primary key(id_vaga),
+id int not null,
+primary key(id),
 foreign key(id_usuario) references first_work.usuario(id),
-foreign key(Cnpj_emp) references first_work.empresa(CNPJ));
+foreign key(id_emp) references first_work.empresa(id));
 
 create table if not exists log_vaga(
-id_log Int not null auto_increment,
+id int primary key auto_increment,
 data_cadastro datetime,
-reg_cnpj int not null,
+i int not null,
 reg_vaga int not null,
-primary key (id_log),
-foreign key (reg_cnpj) references first_work.empresa(CNPJ),
-foreign key (reg_vaga) references first_work.vaga(id_vaga));
+primary key (id),
+foreign key (reg_cnpj) references first_work.empresa(id),
+foreign key (reg_vaga) references first_work.vaga(id));
